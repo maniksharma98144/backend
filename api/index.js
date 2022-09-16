43919ce6c122dde9836a75a786094be741c46473
Manik Sharma <maniksharma98144@gmail.com>
2022-09-16 00:15:30 -0400

    swagger

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.get('/', (req, res) => {
-    res.send("Kindly use the respective APIs, go to: /api/api-docs");
+    res.send("Kindly use the respective APIs, go to: /api-docs");
 })
 
 // api routes for users and tweets
